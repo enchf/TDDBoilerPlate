efaf479fc23e218fc9a08e4f171c42d48352e605
enchf <e.ernesto.espinosa@gmail.com>
2017-02-14 17:13:20 -0600

    Improved lookAndSay

--- 01_lookandsay/lookandsay.js
+++ 01_lookandsay/lookandsay.js
@@ -1,7 +1,16 @@
-function lookAndSay(num) {
-  "use strict";
+"use strict";
+
+let isNumberNonEmpty = (num) => Number.isInteger(num) ? num > 0 : num.length > 0;
+let getLastDigit = (num) =>  Number.isInteger(num) ?
+        [num % 10, Math.floor(num/10)] :
+        [num[num.length - 1], num.substring(0,num.length - 1)].map((i) => +i);
+
+let lookAndSay = (num, i) => i == 0 ? num : lookAndSay(lookAndSayUnary(num),i - 1);
+
+function lookAndSayUnary(num) {
   let last;
   let curr;
+  let destr;
   let result = '';
   let count = 0;
   let add = function() {
@@ -9,9 +18,9 @@ function lookAndSay(num) {
     count = 1;
     last = curr;
   };
-  while (num > 0) {
-    curr = num % 10;
-    num = Math.floor(num/10);
+  while (isNumberNonEmpty(num)) {
+    destr = getLastDigit(num);
+    curr = destr[0]; num = destr[1];
     if (last === undefined) last = curr;
     if (curr != last) add();
     else count++;
@@ -20,7 +29,13 @@ function lookAndSay(num) {
   return result;
 }
 
-console.log(lookAndSay(1));
-console.log(lookAndSay(11));
-console.log(lookAndSay(44445));
-console.log(lookAndSay(111221));
+console.log(getLastDigit("3445"));
+console.log(getLastDigit(3456));
+
+console.log(lookAndSayUnary(1));
+console.log(lookAndSayUnary(11));
+console.log(lookAndSayUnary(44445));
+console.log(lookAndSayUnary(111221)); //312211
+
+console.log(lookAndSay(11,2)); // 1211
+console.log(lookAndSay(111221,2)); // 13112221
